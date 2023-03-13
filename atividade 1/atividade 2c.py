b50cd4a8b7a8f91bea78e6bb364f50157a8f5542
Ecarvalhotec <123981728+Ecarvalhotec@users.noreply.github.com>
2023-03-13 15:48:11 -0300

    Update atividade 2c.py

--- atividade 1/atividade 2c.py
+++ atividade 1/atividade 2c.py
@@ -7,6 +7,7 @@ media = 0
 for i in range(num_alunos):
     nomes.append(input('Informe o nome do aluno: '))
     notas.append(float(input('Informe a nota de ' + nomes[i] + ': ')))
+    media = media + notas[i]
 
 media = media / num_alunos
 print('A media da turma eh ', media)
