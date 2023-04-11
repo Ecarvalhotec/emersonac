202272d6e8798daae43ef3b25e08b2499be6f516
Ecarvalhotec <123981728+Ecarvalhotec@users.noreply.github.com>
2023-04-11 08:50:37 -0300

    test

--- aula 1004/import json.py
+++ aula 1004/import json.py
@@ -1,58 +1,12 @@
 import json
 
-# Definindo as entidades que precisam ser modeladas e seus atributos
-class Funcionario:
-    def __init__(self, id, nome, salario, departamento_id):
-        self.id = id
-        self.nome = nome
-        self.salario = salario
-        self.departamento_id = departamento_id
+with open ('dados.json') as file:
+    data = json.load(file)
 
-class Departamento:
-    def __init__(self, id, nome, localizacao):
-        self.id = id
-        self.nome = nome
-        self.localizacao = localizacao
-
-# Criando um diagrama de relacionamento para representar os relacionamentos entre as entidades
-# O relacionamento é um para muitos: um departamento pode ter vários funcionários, mas cada funcionário só pode estar em um departamento.
-
-# Criando as classes em Python correspondentes às entidades e seus atributos
-# As classes devem ter os mesmos atributos definidos no diagrama de relacionamento.
-class Funcionario:
-    def __init__(self, id, nome, salario, departamento_id):
-        self.id = id
-        self.nome = nome
-        self.salario = salario
-        self.departamento_id = departamento_id
-
-class Departamento:
-    def __init__(self, id, nome, localizacao):
-        self.id = id
-        self.nome = nome
-        self.localizacao = localizacao
-
-# Criando uma lista de objetos das classes criadas com dados fictícios
-# Esses objetos serão usados para criar o arquivo JSON
-funcionarios = [
-    Funcionario(1, "Ana", 2500, 1),
-    Funcionario(2, "Bruno", 3500, 1),
-    Funcionario(3, "Carlos", 4500, 2),
-    Funcionario(4, "Denise", 5500, 2),
-    Funcionario(5, "Eduardo", 6500, 2)
-]
-
-departamentos = [
-    Departamento(1, "Vendas", "São Paulo"),
-    Departamento(2, "Marketing", "Rio de Janeiro")
-]
-
-# Criando um dicionário contendo a lista de objetos e seus atributos
-dados = {
-    "funcionarios": [vars(funcionario) for funcionario in funcionarios],
-    "departamentos": [vars(departamento) for departamento in departamentos]
-}
-
-# Salvando o dicionário em um arquivo JSON
-with open("dados.json", "w") as arquivo_json:
-    json.dump(dados, arquivo_json)
+funcionarios = data['funcionarios']
+print (funcionarios)
+for funcionario in funcionarios:
+    print(f"ID: {funcionario['id']}")
+    print(f"Nome: {funcionario['nome']}")
+    print(f"CPF: {funcionario['salario']}")
+    print(f"ID do departamento: {funcionario['id_departamento']}")
